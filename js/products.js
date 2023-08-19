a5bb2e1445cae37d3a78cf0763b8cbfb32231340
Federico Trotta <ftrotta18@gmail.com>
2023-08-19 19:29:42 -0300

    Fetch products
    1) Quite las concatenaciones de variables javascript con el signo del + y agregue concatenaciones con $ { } asi queda mas legible

--- js/products.js
+++ js/products.js
@@ -8,15 +8,15 @@ async function fetchProducts() {
     <div class="list-group-item list-group-item-action">
         <div class="row">
             <div class="col-3">
-                <img src="` + category.image + `" alt="product image" class="img-thumbnail">
+                <img src="${category.image}" alt="product image" class="img-thumbnail">
             </div>
             <div class="col">
                 <div class="d-flex w-100 justify-content-between">
                     <div class="mb-1">
-                        <h4>`+ category.name +` - ${category.currency} ${category.cost}</h4> 
-                        <p> `+ category.description +`</p> 
+                        <h4> ${category.name} - ${category.currency} ${category.cost}</h4> 
+                        <p>${category.description}</p> 
                     </div>
-                    <small class="text-muted">` + category.soldCount + ` vendidos</small> 
+                    <small class="text-muted"> ${category.soldCount} vendidos</small> 
                 </div>
             </div>
         </div>
