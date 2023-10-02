b4c81e1be1ebc923ce86628069ab9f557ca71552
nahuelbonifacino <nahuelbonifacino98@gmail.com>
2023-10-02 20:34:25 -0300

    funcionalidad a cart.js
    La funcionalidad de calcular el subtotal del producto dependiendo de la cantidad

--- js/cart.js
+++ js/cart.js
@@ -6,19 +6,32 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
   function mostrarProducto (producto) {
+    
     const div = document.getElementById("producto-info");
     let contenidoHtml = `
     <div id="css-articulos">
     <img src="${producto.articles[0].image}">
     <p>${producto.articles[0].name}</p>
     <p>${producto.articles[0].currency} ${producto.articles[0].unitCost}</p>
-    <input type="number" name="contador" value="1">
-    <p class="subtotal">Hola uwu</p>
+    <input id="cantidad" type="number" value="1" min="1">
+    <p class="subtotal"><div id="subtotal"></div></p>
     </div>
     <div class="paso-hr">
     <hr>
     </div>
     `;
     div.innerHTML = contenidoHtml
-    console.log(producto.name)
-  }
+    let cantidadInput = document.getElementById('cantidad');
+    cantidad.addEventListener('change', () => {
+        let cantidad = cantidadInput.value
+        let resultado = subtotal(producto.articles[0].unitCost, cantidad)
+        let intento = `<p>${resultado}</p>`
+        document.getElementById("subtotal").innerHTML = intento
+    }  
+  )}
+
+function subtotal(costo, valor){
+    let costoNumber = Number(costo)
+    let resultado = costoNumber * valor
+    return resultado
+}
